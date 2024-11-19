074bcd174d73b257cc2b5a6b37523300edabc1e5
txrunishere <tsoni9286@gmail.com>
2024-11-19 18:20:04 +0530

    update table format and add post route

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static('./uploads'))
+
 // Templating engine
 app.set("view engine", "ejs");
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,79 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const multer = require('multer');
 
 
-router.get('/', (req, res) => {
-    res.render("index", { title: "Home Page" });
+// Image Upload
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
+    }
 });
 
+var uploads = multer({
+    storage: storage,
+}).single("image");
+
+
+// Index Route 
+router.get('/', async (req, res) => {
+    try {
+        // Fetch users using async/await
+        const users = await User.find().exec();
+
+        // Render the index page with users
+        res.render('index', {
+            title: "Home Page",
+            users: users
+        });
+    } catch (err) {
+        // Handle errors
+        res.json({ message: err.message });
+    }
+});
+
+
+
+/**
+ * GET /
+ * add users
+ */
 router.get("/add", (req, res) => {
     res.render("add-user", { title: "Add User" });
 });
 
-module.exports = router;
 
+/**
+ * POST /
+ * add users
+ */
+router.post('/add', uploads, async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: req.file.filename,
+    });
+    user.save()
+        .then(result => {
+            req.session.message = {
+                type: 'success',
+                message: 'User added successfully'
+            };
+            res.redirect('/'); 
+        })
+        .catch(err => {
+            req.session.message = {
+                type: 'danger',
+                message: err.message
+            };
+            res.redirect('/');
+        });
+});
+
+
+module.exports = router;
